d6d20af1e1d60b8d7155f0ecabc8da1de57c723f
LanceLRG <Gagnerlance@gmail.com>
2021-02-22 13:51:42 -0600

    setup reducers saga and route for getting and storing tasks

---
+++ server/routes/task.router.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/:id', (req, res) => {
+  // GET route code here
+  console.log('incoming id on params is:', req.params);
+  const userId = req.params
+  const queryText = `
+  SELECT "name", "style", "icon", "task"."complete", "date_created", "primary_id", "amount", "unit", "special", "timer", "timer_time", "stopwatch", "stopwatch_time", "task_specs"."complete", "long_streak", "current_streak" FROM "task"
+  JOIN "task_specs" ON "task_specs"."task_id" = "task"."id"
+  JOIN "user" ON "user"."id" = "task"."user_id"
+  WHERE "user_id" = $1;`
+  pool.query(queryText, [userId])
+  .then ((response) => {
+      res.send(response.data)
+  }).catch ((err) => {
+      console.log(err);
+  })
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
---
+++ src/redux/sagas/task.saga.js
@@ -0,0 +1,19 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+// This saga will recieve the id of the user logged in and send out a GET request to retrieve 
+// all the tasks the user associate with that id has, and places them into the task reducer
+function* fetchTask(action) {
+    try{
+        const response = yield axios.get(`/api/task/${action.userId}`)
+        yield put ({type: 'SET_TASK', payload: response.data})
+    } catch (error) {
+        console.log(`error GETTING tasks, ${error}`);
+    }
+}
+
+function* taskSaga() {
+    takeLatest ('FETCH_TASK', fetchTask);
+}
+
+export default taskSaga
