3cd32b27caad2da7f68f73ac84e571b17cb9cb8b
LanceLRG <Gagnerlance@gmail.com>
2021-03-04 10:24:25 -0600

    unset reducers on logout to safeguard against leaks into and between users

--- src/redux/sagas/login.saga.js
+++ src/redux/sagas/login.saga.js
@@ -53,6 +53,10 @@ function* logoutUser(action) {
     // remove the client-side user object to let
     // the client-side code know the user is logged out
     yield put({ type: 'UNSET_USER' });
+    yield put({ type: 'UNSET_EDIT' });
+    yield put({ type: 'UNSET_PRIMARY_HISTORY' });
+    yield put({ type: 'UNSET_PRIMARY_TASK' });
+    yield put({ type: 'UNSET_TASK' });
   } catch (error) {
     console.log('Error with user logout:', error);
   }
