1c9b3516c9662d315f810c93eff51980f7c981e9
LanceLRG <Gagnerlance@gmail.com>
2021-03-05 16:23:35 -0600

    basic timer displaying

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -21,27 +21,7 @@ import Modal from 'react-bootstrap/Modal';
 // import ModalBody from 'react-bootstrap/ModalBody'
 // import ModalFooter from 'react-bootstrap/ModalFooter'
 
-// function timerModal(props){
-//   return(
-//     <Modal {...props} size="lg" centered>
-//       <Modal.Header closeButton>
-//         <Modal.Title id="contained-modal-title-vcenter">
-//           Timer
-//         </Modal.Title>
-//       </Modal.Header>
-//       <Modal.Body>
-//         Hello
-//         {/* <Timer initialtime={specs}>
-//           <Timer.Minutes/>:
-//           <Timer.Seconds/> 
-//         </Timer>*/}
-//       </Modal.Body>
-//       <Modal.Footer>
-//         <Button onClick={props.onHide}>Close</Button>
-//       </Modal.Footer>
-//     </Modal>
-//   )
-// }
+
 
 function UserPage() {
 
@@ -58,6 +38,7 @@ function UserPage() {
   // If the day is marked as 'complete' but there aren't enough 'task-completes'
   // it will set the day's completion back to false.
   const [percent, setPercent] = useState(0);
+  const [myTime, setMytime] = useState('')
   const calcComplete = () => {
     let compCount = 0;
 
@@ -119,44 +100,17 @@ function UserPage() {
     calcComplete();
   }, [store.primaryTask])
 
-  // const [modalShow, setModalShow] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
 
   const myTimer = (myTime) => {
-    console.log('hello');
-    let countdown = (myTime * 60 * 1000)
-    return (
-      <>
-        <Timer initialTime={countdown} direction="backward" startImmediately={false}
-          checkpoints={[
-            {
-              time: ((myTime * 60 * 1000) - 5000),
-              callback: () => console.log('Checkpoint A'),
-            }
-          ]}
-        >
-          {({ start, stop, reset }) => (
-            <React.Fragment>
-              <div>
-                <Timer.Minutes />:
-              <Timer.Seconds />
-              </div>
-              <br />
-              <div>
-                <button onClick={start}>Start</button>
-                <button onClick={stop}>Stop</button>
-                <button onClick={reset}>Reset</button>
-              </div>
-            </React.Fragment>
-          )}
-        </Timer>
-      </>
-    )
-}
+    setModalShow(true);
+    setMytime(myTime)
+  }
 
   const completer = (task) => {
     if (task.timer) {
-      return(
-      <FontAwesomeIcon className="timer-button" icon={['far', `clock`]} size="2x" color="#3298dc" onClick={() => myTimer(task.timer_time)} />
+      return (
+        <FontAwesomeIcon className="timer-button" icon={['far', `clock`]} size="2x" color="#3298dc" onClick={() => myTimer(task.timer_time)} />
       )
     }
     else {
@@ -166,65 +120,117 @@ function UserPage() {
     }
   }
 
-  return (
-    <div className="container">
-      <Container fluid="md">
+  const MyVerticallyCenteredModal = (props) => {
+    let countdown = (myTime * 60 * 1000)
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Timer
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Timer initialTime={countdown} direction="backward" startImmediately={false}
+            checkpoints={[
+              {
+                time: ((myTime * 60 * 1000) - 5000),
+                callback: () => console.log('Checkpoint A'),
+              }
+            ]}
+          >
+            {({ start, stop, reset }) => (
+              <React.Fragment>
+                <div>
+                  <Timer.Minutes />:
+                <Timer.Seconds />
+                </div>
+                <br />
+                <div>
+                  <button onClick={start}>Start</button>
+                  <button onClick={stop}>Stop</button>
+                  <button onClick={reset}>Reset</button>
+                </div>
+              </React.Fragment>
+            )}
+          </Timer>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 
-        {/* <Button variant="info" onClick={() => setModalShow(true)}>Launch Modal</Button>
-        <timerModal show={modalShow} onHide={() => setModalShow(false)}/> */}
-        <div>
-          {/* <button onClick={() => calcComplete()}>Calculate completions</button>
+  return (
+    <>
+      <Button variant="info" onClick={() => setModalShow(true)}>Launch Modal</Button>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      <div className="container">
+        <Container fluid="md">
+
+
+          <div>
+            {/* <button onClick={() => calcComplete()}>Calculate completions</button>
         <button onClick={() => checkDay()}>Check Day</button>
         <button onClick={() => history.push('/calendar')}>Calendar</button> */}
-          <Card style={{ width: '100%' }}>
-            <Card.Header>
-              <h1 className="font-weight-bold">{moment().format('MMMM Do YYYY')}</h1>
-              <h5 className="header text-right">Current Streak: {store.primaryTask.current_streak}</h5>
-              {(store.primaryTask.complete) ? <FontAwesomeIcon id="star" icon={['fas', `star`]} color="#ffbb3e" size="2x" /> : <FontAwesomeIcon id="star" icon={['fas', `star`]} opacity=".2" size="2x" />}
-            </Card.Header>
-            <Card.Body>
-              {/* <Card.Text>Longest Streak: {(store.primaryTask.long_streak)}</Card.Text> */}
-              <ProgressBar variant="info" now={percent} />
-            </Card.Body>
-          </Card>
-        </div>
-        {/* <h2>Welcome, {user.username}!</h2>
-        <p>Your ID is: {user.id}</p> */}
-        {/* draws a task for every task in the task reducer conditionally renders elements
-          based on whether or not certain elements are setup*/}
-        {store.task.map((task) =>
-          <Accordion>
-            <Card style={{ width: '100%', margin: '10px' }}>
-              <div className="task" key={task.id}>
-                <Card.Header>
-                  <Accordion.Toggle className='arrow' eventKey="0">
-                    <OverlayTrigger key="left" placement="left" delay={{ show: 300 }} overlay={<Tooltip id={'tooltip-left'}>details</Tooltip>}>
-                      <FontAwesomeIcon id="icon" icon={['fas', `ellipsis-v`]} size="1x" color="#3298dc" />
-                    </OverlayTrigger>
-                  </Accordion.Toggle>
-                  <FontAwesomeIcon id="icon" icon={['fas', `${task.icon}`]} size="2x" />
-                  <h3 className="task-name">{task.name}</h3>
-                  <OverlayTrigger key="top" placement="top" delay={{ show: 200 }} overlay={<Tooltip id={'tooltip-top'}>edit</Tooltip>}>
-                    <button className="edit-button" onClick={() => handleEdit(task.id)}><FontAwesomeIcon icon={['fas', 'pen']} size="1x" /></button>
-                  </OverlayTrigger>
-                  {(task.tcomplete) ? <button className="btn-secondary undo-button" value={task.id} onClick={(e) => markUndo(e.target.value)}>Undo</button> : completer(task)}
-                  {(task.tcomplete) ? <FontAwesomeIcon id="completion" icon={['far', `check-circle`]} color="green" size="2x" /> : <FontAwesomeIcon id="completion" icon={['far', `circle`]} size="2x" />}
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>{(task.amount) && <p>{task.amount} {task.unit}</p>}
-                    {(task.special) && <p>{task.special}</p>}
-                    {(task.timer) && <p>{task.timer_time} minutes</p>}
-                  </Card.Body>
-                </Accordion.Collapse>
-              </div>
+            <Card style={{ width: '100%' }}>
+              <Card.Header>
+                <h1 className="font-weight-bold">{moment().format('MMMM Do YYYY')}</h1>
+                <h5 className="header text-right">Current Streak: {store.primaryTask.current_streak}</h5>
+                {(store.primaryTask.complete) ? <FontAwesomeIcon id="star" icon={['fas', `star`]} color="#ffbb3e" size="2x" /> : <FontAwesomeIcon id="star" icon={['fas', `star`]} opacity=".2" size="2x" />}
+              </Card.Header>
+              <Card.Body>
+                {/* <Card.Text>Longest Streak: {(store.primaryTask.long_streak)}</Card.Text> */}
+                <ProgressBar variant="info" now={percent} />
+              </Card.Body>
             </Card>
-          </Accordion>
-        )}
-        <br />
-        <br />
-        <button className="btn btn-block btn-outline-primary" onClick={() => history.push('/taskmanage')}>Add task</button>
-      </Container>
-    </div>
+          </div>
+          {/* <h2>Welcome, {user.username}!</h2>
+        <p>Your ID is: {user.id}</p> */}
+          {/* draws a task for every task in the task reducer conditionally renders elements
+          based on whether or not certain elements are setup*/}
+          {store.task.map((task) =>
+            <Accordion>
+              <Card style={{ width: '100%', margin: '10px' }}>
+                <div className="task" key={task.id}>
+                  <Card.Header>
+                    <Accordion.Toggle className='arrow' eventKey="0">
+                      <OverlayTrigger key="left" placement="left" delay={{ show: 300 }} overlay={<Tooltip id={'tooltip-left'}>details</Tooltip>}>
+                        <FontAwesomeIcon id="icon" icon={['fas', `ellipsis-v`]} size="1x" color="#3298dc" />
+                      </OverlayTrigger>
+                    </Accordion.Toggle>
+                    <FontAwesomeIcon id="icon" icon={['fas', `${task.icon}`]} size="2x" />
+                    <h3 className="task-name">{task.name}</h3>
+                    <OverlayTrigger key="top" placement="top" delay={{ show: 200 }} overlay={<Tooltip id={'tooltip-top'}>edit</Tooltip>}>
+                      <button className="edit-button" onClick={() => handleEdit(task.id)}><FontAwesomeIcon icon={['fas', 'pen']} size="1x" /></button>
+                    </OverlayTrigger>
+                    {(task.tcomplete) ? <button className="btn-secondary undo-button" value={task.id} onClick={(e) => markUndo(e.target.value)}>Undo</button> : completer(task)}
+                    {(task.tcomplete) ? <FontAwesomeIcon id="completion" icon={['far', `check-circle`]} color="green" size="2x" /> : <FontAwesomeIcon id="completion" icon={['far', `circle`]} size="2x" />}
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>{(task.amount) && <p>{task.amount} {task.unit}</p>}
+                      {(task.special) && <p>{task.special}</p>}
+                      {(task.timer) && <p>{task.timer_time} minutes</p>}
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </div>
+              </Card>
+            </Accordion>
+          )}
+          <br />
+          <br />
+          <button className="btn btn-block btn-outline-primary" onClick={() => history.push('/taskmanage')}>Add task</button>
+        </Container>
+      </div>
+    </>
   );
 }
 
