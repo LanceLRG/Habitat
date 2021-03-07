ac67d4cfc9a27cdc06556c1e128cc6ef4e06c20a
LanceLRG <Gagnerlance@gmail.com>
2021-03-07 13:26:05 -0600

    added icons to  nav bar and increased logo size

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
   return (
-    <button
-      // This button shows up in multiple locations and is styled differently
-      // because it's styled differently depending on where it is used, the className
-      // is passed to it from it's parents through React props
-      className={props.className}
-      onClick={() => dispatch({ type: 'LOGOUT' })}
-    >
+    <>
+      <div
+        // This div shows up in multiple locations and is styled differently
+        // because it's styled differently depending on where it is used, the className
+        // is passed to it from it's parents through React props
+        className={props.className}
+        onClick={() => dispatch({ type: 'LOGOUT' })}
+      >
+        <FontAwesomeIcon icon={['fas', `sign-out-alt`]} size="2x" />
+        <br />
       Log Out
-    </button>
+    </div>
+    </>
   );
 }
 
