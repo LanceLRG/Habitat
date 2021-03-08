e1749c12aaa4e897de6674afc4c29987b7ea3ecb
LanceLRG <Gagnerlance@gmail.com>
2021-03-08 16:47:53 -0600

    added axios test

--- src/redux/reducers/errors.reducer.test.js
+++ src/redux/reducers/errors.reducer.test.js
@@ -1,7 +1,15 @@
 import { loginMessage } from './errors.reducer';
 
+import axios from 'axios';
+axios.defaults.adapter = require('axios/lib/adapters/http');
+
 test('Test that login reducer can clear correctly', () => {
     expect(loginMessage(undefined, {type: ''})).toBe('');
     expect(loginMessage('', {type: 'CLEAR_LOGIN_ERROR'})).toBe('');
 
 });
+
+test('test that a user can login', async () => {
+    const response = await axios.post('http://localhost:5000/api/user/login', {username: 'example', password: 'example'})
+    expect(response.status).toBe(200);
+})
